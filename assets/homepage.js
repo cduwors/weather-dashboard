a387c4e5bbaad9881ed7608ee0f269b8eec4e7e6
cduwors <christinaduwors@gmail.com>
2022-07-10 14:06:05 -0400

    html/css initial commit

---
+++ assets/homepage.js
@@ -0,0 +1,16 @@
+var cityNameEl = document.querySelector("#city-name");
+var cityHighlightEl = document.querySelector("#city-highlight");
+var fiveDayEl = ("#5-day");
+
+//grab the city name from the url query string
+
+
+//search repo for city name
+
+//if city not accurate then ask for new city
+
+//fetch api information
+
+
+//create span to hold city searched
+//append to cities-container
